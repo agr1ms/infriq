d12f2a7de1965f00d2baddfee2516bd1399a6e03
Agrim <agrimsharma1714@gmail.com>
2026-03-20 04:09:03 +0530

    rm: userId from project schema

--- apps/api/src/schemas/project.schema.ts
+++ apps/api/src/schemas/project.schema.ts
@@ -3,13 +3,11 @@ import z from "zod"
 export const createProjectSchema = z.object({
     name: z.string().min(1, "Project name is required"),
     description: z.string().optional(),
-    userId: z.string().min(1, "User ID is required"),
 })
 
 export const updateProjectSchema = z.object({
     name: z.string().min(1, "Project name is required").optional(),
     description: z.string().optional(),
-    userId: z.string().min(1, "User ID is required").optional(),
 })
 
 export type CreateProjectSchema = z.infer<typeof createProjectSchema>;
