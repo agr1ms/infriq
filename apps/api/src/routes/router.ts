9d9b482c0974833bd5949d53186ca59f47c23b8e
Agrim <agrimsharma1714@gmail.com>
2026-03-26 23:59:19 +0530

    agent creation

--- apps/api/src/routes/router.ts
+++ apps/api/src/routes/router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth.routes.js";
 import projectRoute from "./project.routes.js";
+import schemaRouter from "./schema.routes.js";
 
 const router = Router({ mergeParams: true });
 
 router.use("/auth", authRouter);
 router.use("/projects", projectRoute)
+router.use("/schema", schemaRouter);
 
 export default router;
